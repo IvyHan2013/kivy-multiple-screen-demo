8bab86792bf5507d9327fa3b1f70bc56b75af930
Xingan Wang <wangxgwxg@gmail.com>
2017-10-24 22:32:15 -0700

    ok base class working

--- screens/__init__.py
+++ screens/__init__.py
@@ -1,5 +1,23 @@
 from kivy.uix.screenmanager import Screen
 from kivy.properties import BooleanProperty
+from kivy.lang import Builder
+
+Builder.load_string('''
+<ShowcaseScreen>:
+    ScrollView:
+        do_scroll_x: False
+        do_scroll_y: False if root.fullscreen else (content.height > root.height - dp(16))
+        AnchorLayout:
+            size_hint_y: None
+            height: root.height if root.fullscreen else max(root.height, content.height)
+            GridLayout:
+                id: content
+                cols: 1
+                spacing: '8dp'
+                padding: '8dp'
+                size_hint: (1, 1) if root.fullscreen else (.8, None)
+                height: self.height if root.fullscreen else self.minimum_height
+''')
 
 class ShowcaseScreen(Screen):
     fullscreen = BooleanProperty(False)
